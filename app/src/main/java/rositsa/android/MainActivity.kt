35f82360f80338b0b86398eb655260d69b368f68
RosicaNikolova <rosicanikolova1402@gmail.com>
2023-03-26 21:59:39 +0200

    Dice roll app created with changing pictures

--- app/src/main/java/rositsa/android/MainActivity.kt
+++ app/src/main/java/rositsa/android/MainActivity.kt
@@ -2,7 +2,7 @@ package rositsa.android
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -18,14 +18,26 @@ class MainActivity : AppCompatActivity() {
 //            val toast = Toast.makeText(this, "Dice Rolled!" , Toast.LENGTH_SHORT)
 //            toast.show();
         }
+
+        rollDice()
     }
 
     //functions here, outside of onCreate
     private fun rollDice() {
         val dice = Dice(6);
         val result = dice.roll();
-        val resultTextView: TextView = findViewById(R.id.textView);
-        resultTextView.text = result.toString();
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        val drawableResource = when(result){
+            1-> R.drawable.dice_1
+            2-> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+            6-> R.drawable.dice_6
+            else ->R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 //Log.d("TAG", myDice.roll().toString());
 }
