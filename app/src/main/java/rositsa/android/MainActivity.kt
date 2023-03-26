b03064fe29f8844a41cc5c8ec0ced9a089b4b5ae
RosicaNikolova <rosicanikolova1402@gmail.com>
2023-03-26 21:36:59 +0200

    Dice roll app created

--- app/src/main/java/rositsa/android/MainActivity.kt
+++ app/src/main/java/rositsa/android/MainActivity.kt
@@ -1,11 +1,41 @@
 package rositsa.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    //the entry point of the program
+    //code here
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+//            val toast = Toast.makeText(this, "Dice Rolled!" , Toast.LENGTH_SHORT)
+//            toast.show();
+        }
+    }
+
+    //functions here, outside of onCreate
+    private fun rollDice() {
+        val dice = Dice(6);
+        val result = dice.roll();
+        val resultTextView: TextView = findViewById(R.id.textView);
+        resultTextView.text = result.toString();
+    }
+//Log.d("TAG", myDice.roll().toString());
+}
+
+
+//classes here outside of the main class
+class Dice(private val numSides : Int){
+    fun roll() : Int{
+        return (1..numSides).random();
     }
 }
+
+
